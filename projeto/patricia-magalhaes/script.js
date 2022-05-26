2d0b1d040ba656a95e0a9c534ba7d8cad887ab3d
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2022-05-25 21:31:31 -0300

    Atualizando script.js

--- projeto/patricia-magalhaes/script.js
+++ projeto/patricia-magalhaes/script.js
@@ -43,9 +43,8 @@ botaoAdd.addEventListener("click", (evento) => {
 
     modelo.style.display = "none";
     containerDeTarefas.style.display = "block"; // coloca  div de tarefas com display block para elas aprarecerem na tela por padrao tudo tem display block no html
-
-    form.reset(); // reseta o formulário para n aparecer no input o ultimo texto
   }
+    form.reset(); // reseta o formulário para n aparecer no input o ultimo texto
 
   textoTarefa.addEventListener("click", () => {
     // if(!textoTarefa.classList.contains('checked')) {
