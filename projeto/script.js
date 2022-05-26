da1c0c73db8b46893b20ee26e8938eb35d7851d4
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-05-26 14:06:01 -0300

    Exercicio feito em aula

--- projeto/script.js
+++ projeto/script.js
@@ -6,24 +6,62 @@ const botaoAdd = document.querySelector("#botao_add_id");
 //capturando o botao add
 const listaDeTarefas = document.getElementById("lista_id");
 // capturando a lista que vai guardar as tarefas
-const modelo = document.querySelector('#modelo_id')
+const modelo = document.querySelector("#modelo_id");
+const containerDeTarefas = document.getElementById("tarefas_id");
+const form = document.getElementById("form_id");
+const botaoLimpa = document.getElementById("botao_limpa_id");
+// capturando o formulário
 
 //função de adicionar a partir do click da usuária, um evento
 //mostrar que com o formulário e o evento de submit a tarefa tbm seria criado
 botaoAdd.addEventListener("click", (evento) => {
   evento.preventDefault();
-
   const elementoLista = document.createElement("li"); // cria o item da lista que não existe no HTML
   const textoTarefa = document.createElement("p"); // cria o paragrafo que não existe
   const iconeDeleta = document.createElement("span"); // cria o nome da lista que não está no html
 
-  textoTarefa.innerText = novaTarefaInput.value;
-  iconeDeleta.innerText = "🗑";
+  if (novaTarefaInput.value.trim() === "") {
+    alert("digite alguma tarefa");
+  } else {
+    textoTarefa.innerText = novaTarefaInput.value;
+    // adiciona dentro do paragrafo criado para ser igual ao valor digitado no input
+    iconeDeleta.innerText = "🗑";
+    //adiciona dentro do input criado esse icone de lixeira
 
-  listaDeTarefas.appendChild(elementoLista);
-  elementoLista.appendChild(textoTarefa);
-  elementoLista.appendChild(iconeDeleta);
+    listaDeTarefas.appendChild(elementoLista); //elemento ul do html adotando o item lista criado mais em cima
+    elementoLista.appendChild(textoTarefa);
+    elementoLista.appendChild(iconeDeleta);
 
-  modelo.style.display='none'
-  contai
+    modelo.style.display = "none";
+    containerDeTarefas.style.display = "block"; // coloca div de tarefas com display block para elas aparecerem na tela por padrão tudo tem display block no html.
+
+    form.reset(); // reserta o formulario após clicar no botão da lista
+  }
+
+  textoTarefa.addEventListener("click", () => {
+    /* if (!textoTarefa.classList.contains("checked")) {
+      //verifica se o paragrafo da tarefa criada não tem a classe checked e então adiciona
+      textoTarefa.classList.add("checked");
+    } else {
+      textoTarefa.classList.remove("checked");
+    }
+    //*/ textoTarefa.classList.toggle("checked");
+    //forma mais simples de fazer
+  });
+
+  iconeDeleta.addEventListener("click", () => {
+    elementoLista.remove();
+
+    if (listaDeTarefas.innerText === "") {
+      // se o container de tarfeas estiver vazio
+      modelo.style.display = "flex";
+      containerDeTarefas.style.display = "none";
+    }
+  });
+});
+
+botaoLimpa.addEventListener("click", () => {
+  listaDeTarefas.innerHTML = "";
+  modelo.style.display = "flex";
+  containerDeTarefas.style.display = "none";
 });
